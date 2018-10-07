8f822c524f1832f8f1e0526bcf78acd7bd68f9d6
Moorad <34781684+mooradal@users.noreply.github.com>
2018-10-07 21:07:03 +0100

    Update v2.0 (Not Done)

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-var canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var selected = document.getElementsByClassName("selected");
-var c = canvas.getContext("2d");
+window.addEventListener("scroll",reload())
 
-var BG = new Image();
-var stars1 = new Image();
-BG.src = "Assets/background.jpg";
-stars1.src = "Assets/scanlines.png";
-var opacity = 0;
-
-var Mx = canvas.width / 2;
-var My = canvas.height / 2;
-
-function mouse(e) {
-    Mx = e.clientX;
-    My = e.clientY;
-}
-
-if (selected[0].innerText == "Projects") {
-    BG.src = "Assets/background2.jpg";
-} else if (selected[0].innerText == "Contact") {
-    BG.src = "Assets/background3.jpg";
-}
-
-BG.onload = function() {
-    for (var i = 0; i < 100; i++) {
-        window.setTimeout(function() {
-            c.globalAlpha = opacity;
-            console.log(opacity);
-            opacity++;
-        }, 10 * i);
+function reload() {
+    window.requestAnimationFrame(reload)
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    console.log(scrollTop)
+    if (scrollTop >= window.innerHeight) {
+        document.getElementById("Nav").style.background = "white"
+        document.getElementById("Nav").style.boxShadow = "0 0 20px rgba(0,0,0,0.8)"
+    } else if (scrollTop < window.innerHeight) {
+        document.getElementById("Nav").style.background = "#00000000"
+        document.getElementById("Nav").style.boxShadow = "0 0 0"
     }
-};
-
-main();
-
-document.body.addEventListener("mousemove", mouse);
-
-c.imageSmoothingEnabled = false;
-
-function main() {
-    window.requestAnimationFrame(main);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    c.globalAlpha = opacity / 100;
-    c.drawImage(BG, -canvas.width / 30 + Mx / 30, -canvas.height / 30 + My / 30, canvas.width + canvas.width / 30, canvas.height + canvas.height / 30);
-    c.drawImage(stars1, 0, 0, 1980, 1080);
 }
