6fdcfe92101a3cc3b29b76e07086d2a644870589
Moorad <34781684+mooradal@users.noreply.github.com>
2018-10-09 13:25:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ window.addEventListener("scroll",reload())
 function reload() {
     window.requestAnimationFrame(reload)
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    console.log(scrollTop)
     if (scrollTop >= window.innerHeight) {
         document.getElementById("Nav").style.background = "white"
         document.getElementById("Nav").style.boxShadow = "0 0 20px rgba(0,0,0,0.8)"
